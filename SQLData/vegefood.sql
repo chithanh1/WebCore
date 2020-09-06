9a940fbf227c87a4a25d312677479c86c4c3e62f
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2020-09-06 10:06:27 +0700

    add: home page+SQL database

---
+++ SQLData/vegefood.sql
@@ -0,0 +1,39 @@
+create database VegeFood
+go
+use VegeFood
+go
+
+create table User(
+    Name nvarchar(100) not null,
+	Username nvarchar(100) primary key,
+    Password nvarchar(200) not null,
+	Image nvarchar(200),
+    Birthday datetime,
+    Sex varchar(10) not null,
+    Phone varchar(50) not null,
+    Address nvarchar(50) not null,
+	Type nvarchar(10) not null,
+	CreateAt datetime not null,
+	UpdateAt datetime not null,
+	LastLogin datetime not null,
+    Node nvarchar(100),
+	Status nvarchar(10) not null
+)
+
+create table Category(
+    Id int IDENTITY(1, 1) primary key,
+	Type nvarchar(50) not null,
+	Description nvarchar(100),
+	Node nvarchar(100)
+)
+
+create table Product(
+    Id int IDENTITY(1, 1) primary key,
+	CategoryId int not null,
+	Name nvarchar(50) not null,
+	Amount int not null,
+	Price int not null,
+	Sale int not null,
+	Description nvarchar(100),
+	Status nvarchar(10)
+)
--- VegeFood/Controllers/HomeController.cs
+++ VegeFood/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using VegeFood.Models;
@@ -18,11 +14,13 @@ namespace VegeFood.Controllers
             _logger = logger;
         }
 
+        [Route("/home")]
         public IActionResult Index()
         {
             return View();
         }
 
+        [Route("/privacy")]
         public IActionResult Privacy()
         {
             return View();
